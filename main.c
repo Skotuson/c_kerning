e5ca005e0f2fc74734b80e785d52b388b09c3b7a
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-02-09 16:14:11 +0100

    final impl

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void stripLF ( char * str ) {
+    int l = strlen ( str );
+    if ( str[l - 1] == '\n' )
+        str[l - 1] = '\0';
+}
 
 void kerning ( const char * str ) {
     while ( *str ) 
@@ -10,8 +17,10 @@ void kerning ( const char * str ) {
 int main ( void ) {
     char * line = NULL;
     size_t size = 0;
-    getline ( &line, &size, stdin );    
-    kerning ( line );
+    while (  getline ( &line, &size, stdin ) != -1 ) {
+        stripLF ( line );
+        kerning ( line );
+    }
     free ( line );
     return 0;
 }
